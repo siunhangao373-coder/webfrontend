71270855fd4402879b6a67e36944eca9df1dd2c3
Le Dinh Luc <siunhangao373@gmail.com>
2026-01-09 00:51:15 +0700

    Update style and main js

--- js/main.js
+++ js/main.js
@@ -55,7 +55,7 @@ document.getElementById('orderForm').addEventListener('submit', async function (
 
   if (name.length < 3) return showError("Họ tên phải ít nhất 3 ký tự !");
   if (!phoneRegex.test(phone)) return showError("Số điện thoại phải có 10 số và bắt đầu bằng 0 !");
-  if (!emailRegex.test(email)) return showError("Email không đúng định dạng!");
+  if (!emailRegex.test(email)) return showError("Email không đúng định dạng hoặc bị bỏ trống!");
   if (!address) return showError("Vui lòng nhập địa chỉ!");
   if (!version || !storage) return showError("Vui lòng chọn phiên bản và dung lượng!");
 
